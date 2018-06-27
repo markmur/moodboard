c87b6d766a0a6b307809e25f9dccc9e6fdffe165
Mark Murray <mark.murray.ie@gmail.com>
2018-06-27 22:00:30 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Draggable from 'react-rnd';
+import Dropzone from 'react-dropzone';
+
+class App extends Component {
+  state = {
+    images: []
+  };
+
+  onDrop = acceptedFiles => {
+    console.log(acceptedFiles);
+    this.setState(({ images }) => ({
+      images: [...images, ...acceptedFiles.map(file => file.preview)]
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Moodboard</h1>
+        <Dropzone onDrop={this.onDrop} />
+        {this.state.images.map(image => (
+          <Draggable
+            key={image}
+            lockAspectRatio
+            default={{ x: 0, y: 0, width: 400 }}
+            style={{
+              border: 'solid 1px #ddd'
+            }}
+          >
+            <img draggable="false" src={image} />
+          </Draggable>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
